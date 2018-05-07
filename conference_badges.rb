2b63fcb20cac7479b5c31363e3536171135fdc03
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-07 04:31:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each{ |badges| "Hello, my name is #{badges}."}
-
-
+  counter = 1
+  attendees.each do |badges|
+    puts "Hello, my name is #{badges}."
+  counter += 1
+end
 end
